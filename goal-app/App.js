b769550b2ba74516396b8c38b2ae48c5e6bfd480
sukant43 <sukant98657@gmail.com>
2025-06-11 21:06:28 +0530

    commit

---
+++ goal-app/App.js
@@ -0,0 +1,10 @@
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+
+export default function MyForm() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
